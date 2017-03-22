67b042b3aac83e92210cfaefac4afd32f8659882
Dan Nadler <dnadler87@gmail.com>
2017-03-21 22:58:05 -0400

    initial commit

---
+++ video.py
@@ -0,0 +1,8 @@
+import picamera
+
+camera = picamera.PiCamera()
+camera.rotation=180
+# camera.resolution = (640, 480)
+camera.start_recording('my_video.h264')
+camera.wait_recording(10)
+camera.stop_recording()
